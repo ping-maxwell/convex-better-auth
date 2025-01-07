913681f80eba18926efe025edac5f54e72bf9ede
Multinite <founders.multinite@gmail.com>
2025-01-07 21:17:08 +1000

    fix(ts-config): build output

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,20 @@ import type { Adapter } from "better-auth";
 import type { DB } from "./types";
 import { generateSchema } from "./generate-schema";
 
-
 export const convexAdapter = (db: DB): Adapter => {
-  return {
-    id: "convex",
-    createSchema(options, file) {
-      const code = generateSchema(options.plugins || []);
-      return {
-        code,
-        path: "/convex/schema.ts",
-        append: false,
-        overwrite: true,
-      };
-    },
-  };
+	return {
+		id: "convex",
+		//@ts-ignore
+		create(data) {},
+		//@ts-ignore
+		createSchema(options, file) {
+			const code = generateSchema(options.plugins || []);
+			return {
+				code,
+				path: "/convex/schema.ts",
+				append: false,
+				overwrite: true,
+			};
+		},
+	};
 };
