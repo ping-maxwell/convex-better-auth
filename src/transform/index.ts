8889863181bb1e43f065f3c24478598e5a629e01
ping-maxwell <maxwell.multinite@gmail.com>
2025-01-27 01:03:08 +1000

    chore: cleanup

--- src/transform/index.ts
+++ src/transform/index.ts
@@ -83,68 +83,6 @@ export const createTransform = ({
       }
       return transformedData;
     },
-    convertWhereClause(where: Where[], model: string) {
-      // const schemaModel = getSchema(model);
-      // if (!where) return [];
-      // if (where.length === 1) {
-      // 	const w = where[0];
-      // 	if (!w) {
-      // 		return [];
-      // 	}
-      // 	const field = getField(model, w.field);
-      // 	if (!schemaModel[field]) {
-      // 		throw new BetterAuthError(
-      // 			`The field "${w.field}" does not exist in the schema for the model "${model}". Please update your schema.`,
-      // 		);
-      // 	}
-      // 	if (w.operator === "in") {
-      // 		if (!Array.isArray(w.value)) {
-      // 			throw new BetterAuthError(
-      // 				`The value for the field "${w.field}" must be an array when using the "in" operator.`,
-      // 			);
-      // 		}
-      // 		return [inArray(schemaModel[field], w.value)];
-      // 	}
-      // 	if (w.operator === "contains") {
-      // 		return [like(schemaModel[field], `%${w.value}%`)];
-      // 	}
-      // 	if (w.operator === "starts_with") {
-      // 		return [like(schemaModel[field], `${w.value}%`)];
-      // 	}
-      // 	if (w.operator === "ends_with") {
-      // 		return [like(schemaModel[field], `%${w.value}`)];
-      // 	}
-      // 	return [eq(schemaModel[field], w.value)];
-      // }
-      // const andGroup = where.filter(
-      // 	(w) => w.connector === "AND" || !w.connector,
-      // );
-      // const orGroup = where.filter((w) => w.connector === "OR");
-      // const andClause = and(
-      // 	...andGroup.map((w) => {
-      // 		const field = getField(model, w.field);
-      // 		if (w.operator === "in") {
-      // 			if (!Array.isArray(w.value)) {
-      // 				throw new BetterAuthError(
-      // 					`The value for the field "${w.field}" must be an array when using the "in" operator.`,
-      // 				);
-      // 			}
-      // 			return inArray(schemaModel[field], w.value);
-      // 		}
-      // 		return eq(schemaModel[field], w.value);
-      // 	}),
-      // );
-      // const orClause = or(
-      // 	...orGroup.map((w) => {
-      // 		const field = getField(model, w.field);
-      // 		return eq(schemaModel[field], w.value);
-      // 	}),
-      // );
-      // const clause: SQL<unknown>[] = [];
-      // if (andGroup.length) clause.push(andClause!);
-      // if (orGroup.length) clause.push(orClause!);
-      // return clause;
-    },
     getField,
     getModelName,
     db,
@@ -156,9 +94,7 @@ function withApplyDefault(
   field: FieldAttribute,
   action: "create" | "update"
 ) {
-  if (action === "update") {
-    return value;
-  }
+  if (action === "update") return value;
   if (value === undefined || value === null) {
     if (field.defaultValue) {
       if (typeof field.defaultValue === "function") {
